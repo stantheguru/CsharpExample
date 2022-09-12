66d2c89c165bce8b5f82f8e8f2dec89748d9b1a4
smatuku <smatuku@dewcis.com>
2022-09-12 11:23:01 +0300

    first commit

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username:");
+            string nombre = Console.ReadLine();
+           
+            Console.WriteLine("Tu nombre es "+nombre);
+        }
+    }
+}
