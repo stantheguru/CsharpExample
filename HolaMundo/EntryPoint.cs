ed9c5876948e0671ddc54b0bdd080f20fd32be85
smatuku <smatuku@dewcis.com>
2022-10-07 16:50:55 +0300

    contructors and this

---
+++ HolaMundo/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using PointsAndLines;
+    class EntryPoint
+    {
+    static void Main()
+    {
+        Point point = new Point(5,6);
+        
+        System.Console.WriteLine(point.X);
+        System.Console.WriteLine(point.Y);
+    }
+    }
+
---
+++ HolaMundo/PoinysAndLines/Point.cs
@@ -0,0 +1,17 @@
+﻿namespace PointsAndLines
+{
+    class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+    
+
+}
+
